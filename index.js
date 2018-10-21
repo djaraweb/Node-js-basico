0fe7c980865f5a67139ac6383798dbe26c817388
Deyvi <djara@gmail.com>
2018-10-21 17:54:27 -0500

    Cerrando el Comimit

---
+++ index.js
@@ -0,0 +1,20 @@
+function sumar(a,b) {
+    return a + b;
+}
+
+function restar(a, b){
+    return a - b;
+}
+
+console.log('Bienvenido a la clase de Node');
+console.log(sumar(122,100));
+console.log(restar(100,30));
+
+console.log('Introducción a Node.js: CLI y REPL');
+console.log('En esta clase aprenderemos cómo usar la consola interactiva de Node dónde se puede ejecutar el código de JavaScript');
+
+console.log('REPL (Read-Eval-Print-Loop)');
+console.log('Lee la expresión, evalúa la expresión, Imprime la expresión, Repite la expresión.');
+
+
+console.log('Fin');
